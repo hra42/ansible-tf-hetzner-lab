3b168a2f3d204e4c7801f28a42d5f06e934202fe
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2024-07-25 10:34:46 +0200

    ask user for token, use token inside docker container

--- terraform.sh
+++ terraform.sh
@@ -9,18 +9,26 @@
 # Pull the latest terraform image
 docker pull hashicorp/terraform:latest
 
+# check if TF_VAR_hcloud_token=your_hetzner_cloud_api_token exists
+# if not, ask the user to provide it
+if [ -z "$TF_VAR_hcloud_token" ]; then
+    echo "Please provide your Hetzner Cloud API token:"
+    read -s TF_VAR_hcloud_token
+    export TF_VAR_hcloud_token
+fi
+
 # Allow the user to override the default script with init, plan, apply, or destroy
 if [ "$1" == "init" ]; then
     docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest init
 elif [ "$1" == "plan" ]; then
-    docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest plan
+    docker run --rm -v ${PWD}:/terraform -w /terraform -e TF_VAR_hcloud_token hashicorp/terraform:latest plan -out=tfplan
 elif [ "$1" == "apply" ]; then
-    docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest apply
+    docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest apply tfplan -auto-approve
 elif [ "$1" == "destroy" ]; then
-    docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest destroy
+    docker run --rm -v ${PWD}:/terraform -w /terraform -e TF_VAR_hcloud_token hashicorp/terraform:latest destroy
 else
     docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest init && \
-    docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest plan -out=tfplan && \
+    docker run --rm -v ${PWD}:/terraform -w /terraform -e TF_VAR_hcloud_token hashicorp/terraform:latest plan -out=tfplan && \
     docker run --rm -v ${PWD}:/terraform -w /terraform hashicorp/terraform:latest apply tfplan -auto-approve
 fi
 
